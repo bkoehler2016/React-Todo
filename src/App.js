affeb30f165df9c25cb48451da738d49b9c7e450
Ben Koehler <bkoehler85282@gmail.com>
2020-01-14 13:49:44 -0700

    Completed app with nice styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,83 @@
 import React from "react";
 import Todo from "./components/TodoComponents/Todo";
-import data from "./index.js";
-import TodList from "./components/TodoComponents/TodoList";
+import data from "./components/data";
+import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
+import TodoError from "./components/TodoComponents/TodoError";
 import "./components/TodoComponents/Todo.css";
-
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  constructor() {
+    super();
+    this.state = {
+      taskList: data
+    };
+  }
+
+  finishTask = id => {
+    this.setState({
+      taskList: this.state.taskList.map(item => {
+        if (id === item.id) {
+          console.log("it was clicked!", item.completed); //proof it was clicked and what was the value before the click
+          return {
+            ...item,
+            completed: !item.completed // if the item is not completed complete it and vice versa
+          };
+        }
+        return item;
+      })
+    });
+  };
+
+  addTask = taskText => {
+    let testTask = this.state.taskList.filter(item => {
+      return item.task === taskText;
+    });
+    console.log("testTask Length: ", testTask.lenth);
+
+    if (testTask.length > 0) {
+      this.setState({ error: 1 }); //a repeat has been detected!
+    } else {
+      const newTask = {
+        task: taskText,
+        completed: false,
+        id: Date.now()
+      };
+
+      this.setState({
+        taskList: [...this.state.taskList, newTask],
+        error: 0
+      });
+    }
+  };
+
+  clearTask = id => {
+    this.setState({
+      taskList: this.state.taskList.filter(item => {
+        return item.completed === false;
+      })
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="header">
+          <h1> To Dos..</h1>
+        </div>
+        <br />
+        <div className="list-body">
+          <TodoList
+            taskList={this.state.taskList}
+            finishTask={this.finishTask}
+          />
+          <br />
+          <TodoForm addTask={this.addTask} clearTask={this.clearTask} />
+          <TodoError error={this.state.error} />
+        </div>
       </div>
     );
   }
